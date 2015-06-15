421f0a6d17302a20f8d56f051e379f2717680d17
caldorim <caldorim@gmail.com>
2015-06-15 12:55:35 +0200

    2. Primera página y favicon
    Se añade la primera página y el favicon. Se introducen comentarios en el
    esqueleto.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Importar paquetes con middlewares
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,27 +6,33 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Importar enrutadores
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users'); //no se requiere
 
+//Crear aplicación
 var app = express();
 
 // view engine setup
+//Instalar generador de vistas EJS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+//Instala los middlewares
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Instalar enrutadores
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users); //no se requiere
 
 // catch 404 and forward to error handler
+//Resto de rutas: generar error 404 de HTTP
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
@@ -36,6 +43,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
+//Gestión de errores durante el desarrollo
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -48,6 +56,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
+//Gestión de errores de producción
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
